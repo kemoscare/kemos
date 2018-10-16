95dd100a18bc06403330ac55c4d94d32b8bb130a
Adrien Cantérot <adrien.canterot@gmail.com>
2018-10-16 19:39:45 +0200

    created model.py with mock protocol from mongo and form.io

---
+++ api-kemos/app/protocols/models.py
@@ -0,0 +1,31 @@
+from enum import Enum
+
+class Protocol:
+    def __init__(self):
+        self.theme = ""
+        self.organ = ""
+        self.dayOneEquals = 0
+        self.name = ""
+        self.reevaluation = [] #
+        self.jours = []
+
+
+class Day:
+    def ___init__(self):
+        self.day = 0
+        self.careMode = CareMode(0)
+
+
+
+
+
+
+
+class CareMode(Enum):
+    Admission = "hospitalisation",
+    DayCare = "hdj"
+
+class CareGalenic(Enum):
+    IntraVeinous = "iv",
+    PerOs = "peros",
+    Distributor = "diffusor"
