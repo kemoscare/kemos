a73fc27e3e514b3ab756d42e86e3b766f763e192
Adrien Cantérot <adriencanterot@outlook.com>
2018-10-19 20:00:06 +0200

    adapted API to new form

--- api-kemos/app/protocols/controllers.py
+++ api-kemos/app/protocols/controllers.py
@@ -1,9 +1,10 @@
-from flask import Blueprint, request, jsonify, Response
+from flask import Blueprint, request, jsonify, Response, redirect, url_for
 from pprint import pprint
 from app import MONGODB_CONNECTION, MONGODB_DATABASE_NAME, database
 from . import queries
 from bson.objectid import ObjectId
 from .coders import FormDecoder, FormEncoder
+from pymongo import ReturnDocument
 
 protocols = Blueprint('protocols', __name__, url_prefix='/protocols')
 protocols.json_encoder = FormEncoder
@@ -16,7 +17,6 @@ def get_protocol(id):
     db = database.get_database()
     protocol_id = ObjectId(id)
     res = db.protocoles.find_one({ "_id": protocol_id})
-    res = FormEncoder.encode_formio(res)
     return jsonify(res)
 
 @protocols.route('/names/')
@@ -31,10 +31,13 @@ def get_names():
 def post():
     form_json = request.get_json()
     db = database.get_database()
+    object_id = 0
     if '_id' in form_json:
-        db.protocoles.replace_one({'_id': form_json['_id']}, form_json, True)
+        document = db.protocoles.find_one_and_replace({'_id': form_json['_id']}, form_json, return_document=ReturnDocument.AFTER)
+        object_id = str(document['_id'])
     else:
-        db.protocoles.insert_one(form_json)
-    
-    return Response(status=201)
+        res = db.protocoles.insert_one(form_json)
+        object_id = str(res.inserted_id)
+
+    return redirect(url_for('protocols.get_protocol', id=object_id))
         
