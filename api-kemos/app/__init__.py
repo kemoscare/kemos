1c18d5bc8eac1c7982b881a5ed19b8ae581b9e0d
Adrien Cantérot <adriencanterot@outlook.com>
2018-10-30 13:48:48 +0100

    login manager configuration

--- api-kemos/app/__init__.py
+++ api-kemos/app/__init__.py
@@ -1,16 +1,28 @@
 from flask import Flask
 from flask_cors import CORS
+from flask_login import LoginManager
 
-from config import MONGODB_HOST, MONGODB_DATABASE_NAME, MONGODB_CONNECTION
 from app.protocols.controllers import protocols
 from app.users.controllers import users
+from app.users.models import User
 
-# from app.database.clients import get_client
+
 app = Flask(__name__)
+app.static_url_path = "/"
+login_manager = LoginManager()
+login_manager.init_app(app)
+
 
 # CORS 
-CORS(app)
-CORS(protocols)
+CORS(app, supports_credentials=True)
+
+# Registering blueprints
 app.config.from_object('config')
 app.register_blueprint(protocols)
 app.register_blueprint(users)
+
+# Login Manager
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.get(user_id)
