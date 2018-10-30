9c4bce0a7366237fe72b2976f16152f0fd406432
Adrien Cantérot <adriencanterot@outlook.com>
2018-10-30 13:50:53 +0100

    Logo

---
+++ biglazy-react/src/Logo.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import './Logo.css'
+
+const Logo = () => (
+    <div className="center-title">
+        <span className="KEMOS">KEMOS</span><span className="CARE">.CARE</span>
+    </div>
+)
+
+export default Logo
