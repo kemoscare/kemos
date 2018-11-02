67ac9b68c80ed6c585dfe1a78567dd9225399f07
Adrien Cantérot <adriencanterot@outlook.com>
2018-11-02 11:41:13 +0100

    fixes evaluations not showing if treatment taken at home

--- biglazy-react/src/panes/calculatePlanning.js
+++ biglazy-react/src/panes/calculatePlanning.js
@@ -20,36 +20,40 @@ function parseDays(dayElements) {
 
 function calculateDays(days, startDate) {
     let rdv = parseDays(days)
-    .filter((day) => {return day.careMode != "Home"})
     .map((day) => {
         if(day.day === 1 || day.day === 0) {
             return { date: startDate, type: "Traitement",...day}
         } else {
-            return { date: startDate.add(day.day - 1, 'days'), type: "Traitement", ...day}
+            return { date: moment(startDate).add(day.day - 1, 'days'), type: "Traitement", ...day}
         }
     })
     return rdv
 }
 
 export function calculatePlanning(protocol, startDate) {
+    if(!protocol) return []
     let total = []
     let reevaluations = []
     for(const evaluation of protocol.evaluations) {
         const reevaluationDate = moment(startDate).add((protocol.dayOneEquals*evaluation.dayAfter), 'days').startOf('day')
         reevaluations.push(reevaluationDate.unix())
     }  
-    for(let i=0;i<protocol.evaluations[0].dayAfter*2;i++) {
+
+    let cycles = protocol.evaluations[protocol.evaluations.length-1].dayAfter ? protocol.evaluations[protocol.evaluations.length - 1].dayAfter*2 : 1
+    for(let i=0;i<cycles;i++) {
         let newDate = moment(startDate).add(i*protocol.dayOneEquals, 'days').startOf('day')
         let days = calculateDays(protocol.days, newDate)
         total = total.concat(days)
     }
     total = total.map((day, index) => {
-        if(reevaluations.includes(day.date.unix())) {
+        const reevaluationIndex = reevaluations.indexOf(day.date.unix())
+        if(reevaluationIndex != -1) {
             day.type = "Reevaluation"
-            day.evaluation = protocol.evaluations[0]
+            day.evaluation = protocol.evaluations[reevaluationIndex]
         } 
         day.id = index
         return day
     })     
+    total = total.filter((day) => day.type === "Reevaluation" || day.careMode != "Home")
     return total.sort((d1, d2) => d1.date.unix() - d2.date.unix())
 }
