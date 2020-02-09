e4390ad6de599bd896ef598434e4e3f1b7f5f8a5
Adrien Cantérot <adriencanterot@outlook.com>
2020-02-09 12:10:30 +0100

    Bypass authentication while commiting to this branch

--- biglazy-react/src/authentication.js
+++ biglazy-react/src/authentication.js
@@ -1,6 +1,8 @@
 import _ from 'lodash'
 
 export function hasPermission(rights, user) {
+    // TODO: REMOVE when users reducer is done.
+    return true
     if(!user || ('rights' in user) == false) return false
     return _.intersection(rights, user.rights).length > 0
 }
