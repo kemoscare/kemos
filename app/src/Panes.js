b3438834d085e83844294e6553e000c514c2f096
Adrien Cantérot <adriencanterot@outlook.com>
2020-02-10 00:00:35 +0100

    Drafted new functional Panes to work with redux

---
+++ app/src/Panes.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+export const Panes = ({dispatch}) => (
+
+        <div className="tabs-box">
+            <Tabs className="form-component" id="TabsExample" onChange={(tabId, prev) => dispatch(tabIdSelected(tabId))} large={true} selectedTabId={selectedTabId}>
+                <Tab id="apercu" title="Protocole" panel={<Preview protocol={formContent} loading={chemoLoading} nonIdeal={nonIdeal} />} />
+                <Tab id="pps" title="PPS" panel={<PPS protocol={formContent} />} />
+                <Tab id="patient" title="Patient" panel={<Patient />} />
+                <Tab id="modifier" title="Modifier" panel={<Form /> }/>}/>
+            </Tabs>
+        </div>
+    )
+
