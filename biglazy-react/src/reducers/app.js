7c2e5293fb53876bafbea407c6909ea38ac3252e
Adrien Cantérot <adriencanterot@outlook.com>
2020-02-09 12:14:53 +0100

    default app reducer

---
+++ biglazy-react/src/reducers/app.js
@@ -0,0 +1,21 @@
+/*
+ * AppReducer is the main reducer for the responding to :
+ * - login actions
+ * - back and forth actions with backend such as recording new protocols or else
+ *   @param {object} state The initial state (see further)
+ *   @param {object} action The action to respond to
+ *   @returns {object} Reduced state
+ */
+const initialState = {}
+
+export const app = (state = initialState, action) {
+    switch(action.type) {
+        case REQUEST_PROTOCOL:
+            return state
+        case RECEIVED_PROTOCOL:
+            return state
+        default:
+            return state
+    }
+}
+
