160a4b0b61408b814bc4c736bd22d676c5556db0
Adrien Cantérot <adriencanterot@outlook.com>
2020-03-05 18:27:44 +0100

    new handling of products in days.

--- app/src/panes/Form.js
+++ app/src/panes/Form.js
@@ -15,7 +15,6 @@ import selectContent from './selectContent'
 import DayForm from './FormComponents/DayForm'
 import FormLoading from './FormLoading'
 import AddProtocolForm, { protocolInitialState } from './FormComponents/AddProtocolForm'
-import { daysInitialState } from './FormComponents/DayForm'
 import { daysInitialState } from '../actions/days'
 import { evaluationsInitialState } from './FormComponents/EvaluationForm'
 import { connect } from 'react-redux'
@@ -24,28 +23,30 @@ export const formInitialState = {
     loading: false,
     protocol: protocolInitialState,
     evaluations: evaluationsInitialState,
-    days: daysInitialState
     days: daysInitialState,
     products: {}
 }
-
 
 class Form extends Component {
     render() {
         const { protocol, evaluations, days, dispatch, loading } = this.props
         return (
-            <AddProtocolForm protocol={protocol} days={days} evaluations={evaluations} dispatch={dispatch} loading={loading} />
             <AddProtocolForm { ...this.props}/>
         )
     }
 }
+function productsInDays(products, days) {
+    return days.map(day => { return { ...day, products: products[day.id]}})
+}
 
 function mapStateToProps(state, ownProps) {
-    const { protocol, evaluations, days, loading } = state.editForm
+    const { protocol, evaluations, days, loading, products } = state.editForm
+    const restoredDays = productsInDays(products, days)
+
     return {
         protocol,
         evaluations,
-        days,
+        days: restoredDays,
         loading
     }
 }
