2725069af5c1ca15dfa88cbf74e40e656b23d957
Adrien Cantérot <adriencanterot@outlook.com>
2020-05-10 21:31:17 +0200

    cantarell font like log default

---
+++ landing/src/utils/typography.js
@@ -0,0 +1,16 @@
+import Typography  from 'typography'
+
+const typography = new Typography({
+    baseFontSize: '16px',
+    baseLineHeight: 1.666,
+    googleFonts: [
+        {
+            name: 'Cantarell',
+            styles: ['400']
+        }
+    ],
+    headerFontFamily: ['Cantarell'],
+    bodyFontFamily: ['Cantarell']
+})
+
+export default typography
