4da3e32d59bbfe13727b620b666884a1d299c10e
Adrien Cantérot <adriencanterot@outlook.com>
2020-06-02 07:47:45 +0200

    Grid system for page design

--- landing/src/components/Topbar/Topbar.js
+++ landing/src/components/Topbar/Topbar.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import Button from '../Button'
 import { Link } from 'gatsby'
 import styled from '@emotion/styled'
 import { css } from '@emotion/core'
@@ -8,11 +9,11 @@ import '../../utils/hover.css'
 const Header = styled.div`
    margin:0;
    width: 100%;
-   height: 160px;
+   height: 120px;
     `
 const Logo = styled.img`
-    padding: 30px;
-    width: 400px;
+    margin: 30px 0 0 30px;
+    width: 280px;
     float: left;
     `
 const Links = styled.ul`
@@ -29,35 +30,22 @@ const Li = styled.li`
 
 const link = css`
     display: block;
-    font-size: 18px;
+    font-size: 15px;
     text-transform: uppercase;
     text-decoration: none;
     color: rbg(65, 65, 93);
-    margin: 35px;
+    margin: 35px 20px 30px 20px;
     padding: 3px;
     &:hover {
         box-shadow: inset 0 -3px 0 0 #FE6361; 
     }
     `
 
-const Button = styled.a`
-    display: block;
-    text-transform: uppercase;
-    color: white;
-    font-size: 14pt;
-    margin: 25px;
-    padding: 10px 25px 10px 25px;
-    border-radius: 15px;
-    background-color: #B4D0FD;
-    box-shadow: 0px 0px 25px #BCBCBC;
-    text-decoration: none;
-    `
-    
 const Topbar = () => (
     <Header>
         <Logo src={logo} alt="logo" />
         <Links>
-            <Li><Button href="#">Connexion</Button></Li>
+            <Li><Button href="#" backgroundColor='#B4D0FD'>Connexion</Button></Li>
             <Li><Link className=".underline-from-left" css={link} to="/project">Projet</Link></Li>
             <Li><Link css={link} to="/team">Équipe</Link></Li>
         </Links>
--- landing/src/pages/index.js
+++ landing/src/pages/index.js
@@ -1,15 +1,46 @@
 import React from "react"
 import { Link } from "gatsby"
-
+import Button from '../components/Button'
+import styled from '@emotion/styled'
 import Layout from "../components/layout"
 import SEO from "../components/seo"
+
+
+const Grid = styled.div`
+   display: grid;
+   grid-template-columns: 50% 50%;
+   `
+
+const Right = styled.div`
+    grid-column: 2;
+    background-color: blue;
+    margin: 5em;
+    `
+
+const Left = styled.div`
+    grid-column: 1;
+    background-color: pink;
+    margin: 5em;
+    `
+
+const H3 = styled.h3`
+    text-align: center;
+    `
 
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
+    <Grid>
+        <Left>
+            <H3>Organisation</H3>
+            <p>KEMOS simplifie l'organisation des hôpitaux de jours en proposants divers outils informatisés pour simplifier la prise en charge et le suivi des patients. Nous proposons des solutions intégrées qui respectent les compatibilités existantes avec les standards du marché.</p>
+            
+            <Button href="#" backgroundColor=''>Connexion</Button>
+        </Left>
+        <Right>
+            Ici git l'image du PPS ou un meilleur logo/ icone
+        </Right>
+    </Grid>    
   </Layout>
 )
 
