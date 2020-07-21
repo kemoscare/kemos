f91b60d9b3821ea159f7635a14f0ea42899c1eb5
Adrien Cantérot <adriencanterot@outlook.com>
2020-07-21 15:35:04 +0200

    doc for ADD_FORM_ELEMENT

--- app/src/actions/form.js
+++ app/src/actions/form.js
@@ -97,7 +97,12 @@ export function dateChanged(formName, fieldName, date, fieldId = 0) {
 }
 
 export const ADD_FORM_ELEMENT = 'ADD_FORM_ELEMENT'
-
+/*
+ * Add an element to a form array, providing it with an uuid.
+ * @param formName {string} The name of the array of forms.
+ * @param formState {object} The default state for the form
+ * @return {object} an ADD_FORM_ELEMENT action
+ */
 export function addFormElement(formName, formState) {
     return {
         uuid: uuid(),
